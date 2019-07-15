0860b1276fd2756bbe513d34e3f9bc07c7731355
Uladzimir Papeka <Uladzimir_Papeka@epam.com>
2019-07-15 17:51:32 +0300

    1st load

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77dd957a-dc4d-4d95-952b-c967f958d55f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            RemoteWebDriver Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Driver.Manage().Window.Maximize();
+
+            Driver.Navigate().GoToUrl(@"http://google.com");
+            var targetElement = Driver.FindElement(By.CssSelector("input[aria-label='Search']"));
+            // var targetElement = Driver.FindElementByCssSelector("input[aria-label='Search']"); // Похоже, что то же само
+
+            targetElement.SendKeys("rw.by");
+         
+            //targetElement.Submit();   // Это тоже как Enter, но попробуем найти кнопку и нажать на нее
+            var searchInGoogleButton = Driver.FindElementByCssSelector("center > input[value*='Google']");
+            searchInGoogleButton.Click();
+            
+        }
+    }
+}
